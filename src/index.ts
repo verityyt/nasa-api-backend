a4dc75788035920d3eaf7d0c9a1cf6bca7163567
Verity <joshua-schmidt@online.de>
2021-09-01 17:00:12 +0200

    :lock: Allow cors for verity-network origin

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import express from "express"
 import roverManifestRoute from "./routes/rover/manifest.js"
 import roverPhotosRoute from "./routes/rover/photos.js"
 import dotenv from "dotenv"
+import cors from "cors"
 
 const app = express()
 dotenv.config()
 
 export const apiKey = process.env.api_key
+
+app.use(cors({
+    origin: ["https://verity-network.de", "https://nasa.verity-network.de"]
+}))
 
 app.get("/status", (req, res) => {
     res.send({ status: 200 })
